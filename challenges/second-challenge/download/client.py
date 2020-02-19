2621da71e09d83414beea3e29aead765ebca4ff5
scam627 <stiven.cardona627@gmail.com>
2020-02-18 23:40:36 -0500

    download challenge with hasing verify

--- challenges/second-challenge/download/client.py
+++ challenges/second-challenge/download/client.py
@@ -1,4 +1,5 @@
 import zmq
+import hashlib
 from zhelpers import socket_set_hwm, zpipe
 
 ctx = zmq.Context()
@@ -11,10 +12,11 @@ dealer.send(b"fetch")
 total = 0       # Total bytes received
 chunks = 0      # Total chunks received
 outfile = open("testout", "wb")
+check = hashlib.sha256()
 
 while True:
     try:
-        chunk = dealer.recv()
+        chunk, h = dealer.recv_multipart()
     except zmq.ZMQError as e:
         if e.errno == zmq.ETERM:
             print(e.errno)   # shutting down, quit
@@ -26,6 +28,9 @@ while True:
     if size == 0:
         break   # whole file received
     else:
+        check.update(chunk)
+        if check.digest() != h:
+            break
         outfile.write(chunk)
 print("%i chunks received, %i bytes" % (chunks, total))
 pipe.send(b"OK")
--- challenges/second-challenge/download/server.py
+++ challenges/second-challenge/download/server.py
@@ -1,7 +1,8 @@
 import zmq
+import hashlib
 from zhelpers import socket_set_hwm, zpipe
 
-CHUNK_SIZE=250000
+CHUNK_SIZE = 250000
 
 ctx = zmq.Context()
 
@@ -22,9 +23,11 @@ while True:
             raise
 
     assert command == b"fetch"
+    h = hashlib.sha256()
     while True:
         data = file.read(CHUNK_SIZE)
-        router.send_multipart([identity, data])
+        h.update(data)
+        router.send_multipart([identity, data, h.digest()])
         if not data:
             break
 ctx.term()
