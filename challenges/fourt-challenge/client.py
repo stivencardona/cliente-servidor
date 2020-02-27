209ac4fc8893d73ccc2d8f04d4edd432fda524be
scam627 <stiven.cardona627@gmail.com>
2020-02-26 19:14:32 -0500

    upload feature update

--- challenges/fourt-challenge/client.py
+++ challenges/fourt-challenge/client.py
@@ -7,42 +7,56 @@ class Client:
 		self.context = zmq.Context()
 		self.socket = self.context.socket(zmq.REQ)
 		self.CHUNKSIZE = CHUNKSIZE
+		self.segment_list = []
 	
+	def get_hash(self, data):
+		return hashlib.sha256(data)
+
 	def set_proxy(self, proxy_ip = "localhost",proxy_port = 5556):
 		self.proxy_ip = proxy_ip
 		self.proxy_port = proxy_port
-	
+
 	def download(self):
 		print("Something")
 
 	def send_segment(self, data, server_ip, server_port):
-		self.socket.connect("tcp://%s:%s" % (server_ip, server_port))
-		print("Connect")
-		h = hashlib.sha256()
-		h.update(data)
+		addr = "tcp://{}:{}".format(server_ip, server_port)
+		self.socket.connect(addr)
+		print("Connect to {}".format(addr))
+		h = self.get_hash(data)
 		self.socket.send_multipart([b"upload", data, h.digest()])
 		message = self.socket.recv()
-		print(message)
+		if message == b"ok":
+			self.segment_list.append(addr)
+		self.socket.disconnect(addr)
+		return h.hexdigest()
 
 	def upload(self, filename):
-		addr = "tcp://%s:%i" % (self.proxy_ip , self.proxy_port)
+		addr = "tcp://{}:{}".format(self.proxy_ip , self.proxy_port)
 		self.socket.connect(addr)
 		self.socket.send(b"list")
 		server_list = json.loads(self.socket.recv().decode('utf8'))
 		self.socket.disconnect(addr)
 		path = "storage-client/" + filename
-		file = open(path,"rb")
-		chunk = 0
-		total = 0
-		data = file.read(self.CHUNKSIZE)
-		sz = len(server_list)
-		while data:
-			idx = chunk % sz
-			self.send_segment(data, server_list[idx][0], server_list[idx][1])
-			chunk += 1
-			total += len(data)
-			print("%i chunks send, %i bytes" % (chunk, total))
-			data = file.read(self.CHUNKSIZE)
+		self.segment_list = []
+		with open(path,"rb") as f:
+			chunk = 0
+			total = 0
+			data = f.read(self.CHUNKSIZE)
+			sz = len(server_list)
+			while data:
+				idx = chunk % sz
+				self.send_segment(data, server_list[idx][0], server_list[idx][1])
+				chunk += 1
+				total += len(data)
+				print("{} chunks send, {} bytes".format(chunk, total))
+				data = f.read(self.CHUNKSIZE)
+		self.socket.connect(addr)
+		self.socket.send(b"upload")
+		self.socket.recv()
+		self.socket.send(json.dumps([filename, self.segment_list]).encode('utf8'))
+		self.socket.recv()
+		self.socket.disconnect(addr)
 		print("Upload complete")
 
 client = Client(10485760)
--- challenges/fourt-challenge/proxy.py
+++ challenges/fourt-challenge/proxy.py
@@ -11,12 +11,20 @@ class Proxy:
 		self.commands = {
 			b"register": self.store_server,
 			b"list": self.get_server_list,
-			b"upload": self.store_file
+			b"upload": self.store_file,
+			b"file": self.get_file
 		}
 		self.list_files = {}
 	
+	def get_file(self, filename):
+		self.socket.send(json.dumps(self.list_files[filename]))
+
 	def store_file(self):
-		print("Something")
+		self.socket.send(b"ok")
+		data = json.loads(self.socket.recv().decode('utf8'))
+		self.list_files[data[0]] = data[1]
+		self.socket.send(b"ok")
+		self.get_file()
 
 	def store_server(self):
 		print("Register server")
@@ -28,10 +36,10 @@ class Proxy:
 		self.socket.send(b"ok")
 	
 	def get_server_list(self):
-		self.socket.send(bytes(json.dumps(self.server_list), 'utf8')) 
+		self.socket.send(json.dumps(self.server_list).encode('utf8')) 
 
 	def up(self):
-		print("Server listenning on %i" % self.port)
+		print("Server listenning on {}".format(self.port))
 		while True:
 			command = self.socket.recv()
 			self.commands[command]()
